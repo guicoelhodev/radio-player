73711e429b320bfd7dd5aab1a79f96cdef86e335
guicoelhodev <gs.coelho_dev@outlook.com>
2025-05-14 22:32:38 -0300

    feat(ImageView): create ImageView component

---
+++ src/lib/components/ImageView.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { MusicState } from '$lib/state/MusicState/index.svelte';
+	import type { PlaylistName } from '$lib/state/types/playlist.enum';
+
+	const musicState = MusicState.getInstance();
+	const currentMusic = musicState.getCurrentAudio();
+
+	const videoImage: Record<PlaylistName, { src: string }> = {
+		'Houston Blues Radio': {
+			src: '/videos/jake_music.mp4'
+		},
+		'Synthwave City FM - New': {
+			src: '/videos/cyberpunk_night.mp4'
+		},
+		'Splash Coffee': {
+			src: '/videos/morning_coffee.mp4'
+		},
+		'lofi girl': {
+			src: '/videos/lofi_girl.mp4'
+		},
+		Chillofi: {
+			src: '/videos/cozy_room.mp4'
+		}
+	};
+
+	let videoSelected = $derived(videoImage[currentMusic.radioName as PlaylistName]);
+
+	$inspect(videoSelected);
+</script>
+
+<div>
+	<video
+		autoplay
+		muted
+		loop
+		playsinline
+		class="absolute top-0 left-0 z-[-1] size-full object-cover brightness-75"
+	>
+		<source src={videoSelected.src} type="video/mp4" />
+	</video>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import RadioPlayer from '$lib/components/RadioPlayer.svelte';
 	import Menu from '$lib/components/Menu/index.svelte';
+	import ImageView from '$lib/components/ImageView.svelte';
 </script>
 
+<ImageView />
 <RadioPlayer />
 <Menu />
