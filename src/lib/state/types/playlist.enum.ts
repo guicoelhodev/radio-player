a3db68175bce6a3513750575776725f1f26a2f6e
guicoelhodev <gs.coelho_dev@outlook.com>
2025-05-14 21:02:48 -0300

    feat(global): create PlaylistName Enum to lead with playlist list

---
+++ src/lib/state/types/playlist.enum.ts
@@ -0,0 +1,8 @@
+export enum PlaylistName {
+	Chillofi = 'Chillofi',
+	SplashCoffee = 'Splash Coffee',
+	LofiGirl = 'lofi girl',
+	Synthwave = 'Synthwave City FM - New',
+	Nightride = 'Nightride FM',
+	HoustonBlues = 'Houston Blues Radio'
+}
